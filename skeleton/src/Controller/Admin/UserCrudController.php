824d9a7017c37fe166173c598c6dacef090cdf47
artourtkanov <artur.tkanov@gmail.com>
2025-03-25 15:18:58 +0100

    correction des bugs

--- skeleton/src/Controller/Admin/UserCrudController.php
+++ skeleton/src/Controller/Admin/UserCrudController.php
@@ -7,6 +7,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,14 +20,29 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->remove(Crud::PAGE_INDEX, Action::NEW);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('username'),
+            ChoiceField::new('roles')
+                ->setChoices([
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ])
+                ->allowMultipleChoices()
+                ->renderExpanded(false)
+                ->renderAsBadges([
+                    'ROLE_USER' => 'success',
+                    'ROLE_ADMIN' => 'warning',
+                ]),
         ];
     }
-    */
+
 }
