eb0d75d4c50d30d334b1d6ad155cb0273c6f2a27
artourtkanov <artur.tkanov@gmail.com>
2025-03-25 15:51:03 +0100

    correction des bugs

--- skeleton/src/Controller/Admin/CategoryCrudController.php
+++ skeleton/src/Controller/Admin/CategoryCrudController.php
@@ -7,9 +7,22 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CategoryCrudController extends AbstractCrudController
 {
+
+    public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        try {
+            parent::deleteEntity($entityManager, $entityInstance);
+        } catch (ForeignKeyConstraintViolationException $e) {
+            $this->addFlash('danger', 'Impossible de supprimer cette catégorie car elle est liée à des produits.');
+        }
+    }
     public static function getEntityFqcn(): string
     {
         return Category::class;
